9513e739f1c9fa5f06f8d77cb99c15c95f2d094c
hieuhn <hieungochoangit@gmail.com>
2023-02-27 02:03:27 +0700

    Added loading component

---
+++ frontend/src/components/Loading/Loading.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PulseLoader from 'react-spinners/PulseLoader';
+
+const Loading = () => {
+  return <PulseLoader size={8} color="#FFFFFF" />;
+};
+
+export default Loading;
